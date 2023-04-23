f167725cbc58dde28dd14ecb6c8846e99cd9fb25
abcxyz31 <unnatimotiramani@gmail.com>
2023-04-23 20:47:48 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const leaderboardRoutes = require('./routes/leaderboardaRoutes');
 const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
@@ -28,6 +29,6 @@ app.use('/auth' , authRoutes);
 
 app.use('/leaderboard', leaderboardRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log('Server listening on port 5000');
 });
