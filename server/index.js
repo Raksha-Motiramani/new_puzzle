a51d362851580ae0a5a6609ec43feb8b5a6d6498
abcxyz31 <unnatimotiramani@gmail.com>
2023-04-23 20:40:21 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+//0UCBBODci7RtqgTc
+//puzzle
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const leaderboardRoutes = require('./routes/leaderboardaRoutes');
+const MONGO_URI = process.env.MONGO_URI;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.use('/auth' , authRoutes);
+
+app.use('/leaderboard', leaderboardRoutes);
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
